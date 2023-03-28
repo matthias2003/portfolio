df5fdce0e88d80d4ccda0cd3242bdbf5744fc1b7
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-03-28 21:02:37 +0200

    Added layout to navbar, underline on hover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import HomePage from './Components/HomePage/HomePage.js'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <main>
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </main>
+    </Router>
+    
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,26 @@
+import logo from '../../Assets/logo.png';
+import { Link } from 'react-router-dom';
+import githubIcon from '../../Assets/github.svg';
+import './Nav.css';
+
+function Nav() {
+    return(
+        <nav className='nav'>
+            <div className='container'>
+                <Link to='/'><img className='nav__logo' src={logo} alt='Logo'/></Link>
+                <div className='nav__links'>
+                    <ul>
+                        <li><Link to='/'>HOME</Link></li> {/* TODO: zrobić sciezki i przekierowania na elementy*/}
+                        <li><Link to='/'>ABOUT</Link></li>
+                        <li><Link to='/'>PORTFOLIO</Link></li>
+                        <li><Link to='/'>CONTACT ME</Link></li>
+                    </ul>
+                    <img onClick={() => { window.location.href = 'https://github.com/matthias2003'}} className='nav__icon' src={githubIcon} alt='Github' />
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Nav;
