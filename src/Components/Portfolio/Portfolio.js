0aae6a7fe281eb6ab2a2f836d275699ec8eb584a
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-06-28 21:57:25 +0200

    Made responsive tiles for potrfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,10 +1,26 @@
+import './Portfolio.css';
+
 function Portfolio() {
     return(
         <div className="tiles__wrap">
-            <div className="tiles"></div>
-            <div className="tiles"> </div>
-            <div className="tiles"></div>
-            <div className="tiles"></div>
+            <div className="tiles">
+                <p>Superhero Api</p>
+                <div className='image__box' alt="">
+                    <img src='' />
+                </div>
+            </div>
+            <div className="tiles">
+                <p>Weather App</p>
+                <div className='image__box'></div>
+            </div>
+            <div className="tiles">
+                <p>Todo List</p>
+                <div className='image__box'></div>
+                </div>
+            <div className="tiles">
+                <p>Tv Maze</p>
+                <div className='image__box'></div>
+                </div>
         </div>
     )
 }
