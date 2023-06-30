72c260cf1dc5c2c399313ffc2697b85b8342a145
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-06-30 12:30:11 +0200

    Added style to tiles

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,30 @@
-import './About.css'
+import "./About.css";
 
 function About() {
-    return(
-        <div className='about__wrap'>
-            <h2>So, who am I?</h2>
-            <p>My name is Maciej Kłoda. I'm a Front-End Web Developer working in React JS.</p>
-        </div>
-    )
+  return (
+    <div className="about__wrap">
+      <h2>So, who am I?</h2>
+      <div className="about__cont">
+        <p>
+          My name is Maciej Kłoda. I'm a Front-End Web Developer working in
+          React JS.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          dignissim, enim quis auctor elementum, nisi ligula feugiat urna, nec
+          vehicula risus libero at lacus. Integer sed dolor ante. Ut id leo
+          nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          at sapien eget nunc euismod fringilla ut non arcu. Sed convallis vel
+          risus vitae condimentum. Maecenas in aliquam mi. In massa lacus,
+          vestibulum et diam at, dignissim semper diam. Maecenas et lacinia
+          enim. Cras lobortis eros in nisl commodo tristique. Suspendisse a
+          neque est. Phasellus hendrerit lobortis erat, sed eleifend ipsum
+          dictum consectetur. Aliquam sit amet rhoncus quam. Morbi erat nunc,
+          mollis posuere egestas et, ullamcorper ut odio.
+        </p>
+      </div>  
+    </div>
+  );
 }
 
 export default About;
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,14 @@
-function Contact() { 
+import './Contact.css';
 
+function Contact() {
+    return(
+        <div className='contact__wrap'>
+            <h2>Get in touch with me!</h2>
+            <div className='form__wrap'>
+
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
