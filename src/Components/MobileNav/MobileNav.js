2ae4f159c994d57d260282f36d4720b856353581
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-07-18 20:32:38 +0200

    Changed height of nav, added hamburger icon on mobile nav

--- src/Components/MobileNav/MobileNav.js
+++ src/Components/MobileNav/MobileNav.js
@@ -1,9 +1,15 @@
-import 'MobileNav.css';
-
+import './MobileNav.css';
+import { Link } from 'react-router-dom';
+import hamburger from '../../Assets/hamburger.png';
+import logo from '../../Assets/logo.png';
 
 function MobileNav() {
-
-
+    return(
+        <div className='container'>
+            <img className='nav__mobile__hamburger' src={hamburger} />
+            <Link to='/'><img className='nav__mobile__logo' src={logo} alt='Logo'/></Link>
+        </div>
+    )
 }
 
 export default MobileNav;
