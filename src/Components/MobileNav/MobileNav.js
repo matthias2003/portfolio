94883008acec3d2fac2297e968f8dfd8cd9d711d
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-07-24 12:41:04 +0200

    Added nav handle

--- src/Components/MobileNav/MobileNav.js
+++ src/Components/MobileNav/MobileNav.js
@@ -4,9 +4,15 @@ import hamburger from '../../Assets/hamburger.png';
 import logo from '../../Assets/logo.png';
 
 function MobileNav() {
+
+    const sideNavHandle = () => {
+        
+    }
+
+
     return(
         <div className='container'>
-            <img className='nav__mobile__hamburger' src={hamburger} />
+            <img onClick={sideNavHandle} className='nav__mobile__hamburger' src={hamburger} />
             <Link to='/'><img className='nav__mobile__logo' src={logo} alt='Logo'/></Link>
         </div>
     )
