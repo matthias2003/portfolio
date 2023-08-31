7c9a32e6e2dcb1bbc068eba09cc38ac1c1c37ba4
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-08-31 16:55:53 +0200

    Added errro handling to image preview

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -28,13 +28,11 @@ function Portfolio() {
         img = img.slice(0, -1);
       }
       imgLink[item.name] = regex.exec(img)
-      //imgLink.push(regex.exec(img));
     }
     setPortfolioPreview(imgLink);
   }
 
   console.log(portfolioPreview)
-  
   
 
   return (
@@ -44,39 +42,6 @@ function Portfolio() {
         )}
     </div>
   )
-
-  // return (
-  //   <div className="tiles__wrap">
-      // <div className="tiles">
-      //   <p>Superhero App</p>
-      //   <div className="image__box" alt="">
-      //     <Link to={`/portfolio/1`}>
-      //       {" "}
-      //       <img src={thumbnail.superHeroApp} alt="SuperHeroApp screenshot" />
-      //     </Link>
-      //   </div>
-      // </div>
-
-  //     <div className="tiles">
-  //       <p>Weather App</p>
-  //       <div className="image__box">
-  //         <img src={thumbnail.weatherApp} alt="WeatherApp screenshot" />
-  //       </div>
-  //     </div>
-  //     <div className="tiles">
-  //       <p>Todo List</p>
-  //       <div className="image__box">
-  //         <img src={thumbnail.todoList} alt="TodoList screenshot" />
-  //       </div>
-  //     </div>
-  //     <div className="tiles">
-  //       <p>Tv Maze</p>
-  //       <div className="image__box">
-  //         <img src={thumbnail.movieApp} alt="TvMaze screenshot" />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default Portfolio;
