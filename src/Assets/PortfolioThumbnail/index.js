ce13b055b320cb42b6774daff1f1ed7a588f0a16
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-08-31 17:17:36 +0200

    Changed the way of getting images for protfolio projects prewiev

--- src/Assets/PortfolioThumbnail/index.js
+++ src/Assets/PortfolioThumbnail/index.js
@@ -1,4 +1,4 @@
-export const movieApp = require('./movieapp.png');
-export const superHeroApp = require('./superhero.png');
-export const todoList = require('./todolist.png');
-export const weatherApp = require('./weatherapp.png');
+export const tvMaze = require('./tvMaze.png');
+export const heroApp = require('./heroApp.png');
+export const todoList = require('./todoList.png');
+export const weatherApp = require('./weatherApp.png');
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,8 +1,9 @@
 import "./Portfolio.css";
 import SimplifiedPortfolio from "../SimplifiedPortfolio/SimplifiedPortfolio.js";
-import { getGitHubRepos, getGithubImages } from "../../requests.js";
+import { getGitHubRepos } from "../../requests.js";
 import { useEffect, useState } from "react";
 import emptyPhoto from "../../Assets/emptyPhoto.png";
+import * as preview from "../../Assets/PortfolioThumbnail/index.js";
 
 function Portfolio() {
   useEffect(() => {
@@ -10,35 +11,15 @@ function Portfolio() {
   }, []);
 
   const [portfolioData, setPortfolioData] = useState({});
-  const [portfolioPreview, setPortfolioPreview] = useState([]);
-
   const fetchRepos = async () => {
     const data = await getGitHubRepos();
     setPortfolioData(data);
-    fetchReposPreviews(data);
   };
-
-  const fetchReposPreviews = async ( repoData ) => {
-    let imgLink = {};
-
-    for (const item of repoData) {
-      let img = await getGithubImages(item.name);
-      const regex = new RegExp('https://github.com/matthias2003/.+');
-      if(img) {
-        img = img.slice(0, -1);
-      }
-      imgLink[item.name] = regex.exec(img)
-    }
-    setPortfolioPreview(imgLink);
-  }
-
-  console.log(portfolioPreview)
-  
 
   return (
     <div className="tiles__wrap">
       {Object.entries(portfolioData).map( item =>
-        <SimplifiedPortfolio name={item[1].name} key={item[1].id} img={portfolioPreview[item[1].name]??emptyPhoto}/>
+        <SimplifiedPortfolio name={item[1].name} key={item[1].id} img={preview[item[1].name]??emptyPhoto}/>
         )}
     </div>
   )
