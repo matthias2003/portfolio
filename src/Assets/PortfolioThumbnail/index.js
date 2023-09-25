5e5e3dc624b26c784eb095a41664e705c111de7c
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-09-25 19:20:47 +0200

    Try to fix vercel error

--- src/Assets/PortfolioThumbnail/index.js
+++ src/Assets/PortfolioThumbnail/index.js
@@ -1,4 +1,4 @@
 export const tvMaze = require('./tvMaze.png');
 export const heroApp = require('./heroApp.png');
-export const todoList = require('./todoList.png');
+export const todoList = require('./todolist.png');
 export const weatherApp = require('./weatherApp.png');
