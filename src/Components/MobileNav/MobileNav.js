4cf5c634a02127736217a666f163825bf5eba7c1
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-09-26 16:26:03 +0200

    Fixed style for the mobile app

--- src/Components/MobileNav/MobileNav.js
+++ src/Components/MobileNav/MobileNav.js
@@ -19,20 +19,20 @@ function MobileNav() {
 
     return(
         <>
-        <div className='container'>
-            <Link to='/' onClick={() => sideNavHandle('close')}><img className='nav__mobile__logo' src={logo} alt='Logo'/></Link>
-            <img onClick={() => sideNavHandle('open')} className='nav__mobile__hamburger' src={hamburger} />
-        </div>
-        <div className={menuIsClicked ? 'container__side__nav active' : 'container__side__nav'}> 
-            <img onClick={() => sideNavHandle('close')} className='nav__mobile__close' src={close} alt='Close icon'/>
-            <ul>
-                <li onClick={() => sideNavHandle('close')}><Link to='/'>HOME</Link></li>
-                <li onClick={() => sideNavHandle('close')}><Link to='/about' >ABOUT</Link></li>
-                <li onClick={() => sideNavHandle('close')}><Link to='/portfolio'>PORTFOLIO</Link></li>
-                <li onClick={() => sideNavHandle('close')}><Link to='/contact'>CONTACT ME</Link></li>
-            </ul>
-        </div>
-    </>
+            <div className='container'>
+                <Link to='/' onClick={() => sideNavHandle('close')}><img className='nav__mobile__logo' src={logo} alt='Logo'/></Link>
+                <img onClick={() => sideNavHandle('open')} className='nav__mobile__hamburger' src={hamburger} />
+            </div>
+            <div className={menuIsClicked ? 'container__side__nav active' : 'container__side__nav'}> 
+                <img onClick={() => sideNavHandle('close')} className='nav__mobile__close' src={close} alt='Close icon'/>
+                <ul>
+                    <li onClick={() => sideNavHandle('close')}><Link to='/'>HOME</Link></li>
+                    <li onClick={() => sideNavHandle('close')}><Link to='/about' >ABOUT</Link></li>
+                    <li onClick={() => sideNavHandle('close')}><Link to='/portfolio'>PORTFOLIO</Link></li>
+                    <li onClick={() => sideNavHandle('close')}><Link to='/contact'>CONTACT ME</Link></li>
+                </ul>
+            </div>
+        </>
     )
 }
 
