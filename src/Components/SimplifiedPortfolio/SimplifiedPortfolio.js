3c39423c8f5b1189bc15dabf6c125d94108e1267
matthias03 <maciej.kloda98@gmail.com>
2023-11-06 22:35:07 +0100

    Changed home page picture

--- src/Components/SimplifiedPortfolio/SimplifiedPortfolio.js
+++ src/Components/SimplifiedPortfolio/SimplifiedPortfolio.js
@@ -1,6 +1,16 @@
 import "./SimplifiedPortfolio.css";
 
 function SimplifiedPortfolio({ name, img}) {
+  if (name === "heroApp") {
+    return (
+      <div className="tiles" onClick={() => { window.location.href = `https://hero-app-six.vercel.app`}}>
+        <p>{name}</p>
+        <div className="image__box" >
+          <img src={img} alt="Preview"/>
+        </div>
+      </div>
+    )
+  } else { 
   return (
       <div className="tiles" onClick={() => { window.location.href = `https://${name}-kloda.on.fleek.co`}}>
         <p>{name}</p>
@@ -8,7 +18,7 @@ function SimplifiedPortfolio({ name, img}) {
           <img src={img} alt="Preview"/>
         </div>
       </div>
-  );
+  )};
 }
 
 export default SimplifiedPortfolio;
